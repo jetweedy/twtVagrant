03659093649039470251cf44a1d640cf4d3a7a18
Jon Tweedy <jetweedy@ad.unc.edu>
2020-06-12 08:45:15 -0400

    m

--- resources/lamp-setup.sh
+++ resources/lamp-setup.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
+
 # INSTALL PHP
 echo "\n" | sudo apt-add-repository ppa:ondrej/php
-sudo apt-get update -y
-sudo apt-get install -y php7.1 php7.1-fpm php7.1-cli php7.1-dev php7.1-common php7.1-mbstring php7.1-gd php7.1-intl php7.1-xml php7.1-mysql php7.1-mcrypt php7.1-zip php7.1-sqlite3 php7.1-curl
-sudo apt-get install -y libapache2-mod-php7.1
-sudo apt-get install -y php-pear
-sudo pecl install -y stats
-sudo apt-get install -y phpunit
-sudo apt-get install -y php7.1-imap
-sudo apache2ctl graceful
-sudo a2enmod proxy_fcgi setenvif
-sudo a2enconf php7.1-fpm
-sudo service apache2 restart
-#sudo service apache2 reload
+sudo apt-get update
+sudo apt-get install -y php7.4 php7.4-fpm php7.4-cli php7.4-common php7.4-mbstring php7.4-gd php7.4-intl php7.4-xml php7.4-mysql php7.4-zip php7.4-sqlite3 php7.4-curl php-pear php7.4-dev libapache2-mod-php7.4
 
-cp /vagrant/phpHelloWorld.php /var/www/html/phpHelloWorld.php
+# INSTALL APACHE
+sudo apt-get install apache2 -y
+sudo a2enmod rewrite
+sudo service apache2 restart
+
+# INSTALL MSQL
+#export DEBIAN_FRONTEND=noninteractive
+#sudo apt-get install -y mysql-server
+#sudo mysql < /vagrant/setup.sql
+
