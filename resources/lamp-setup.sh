1fa9fb8b907ab499fe511ddb0b89dc8c6565aa2f
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2021-08-10 09:52:39 -0400

    Update lamp-setup.sh

--- resources/lamp-setup.sh
+++ resources/lamp-setup.sh
@@ -9,6 +9,8 @@ sudo apt install  ca-certificates apt-transport-https software-properties-common
 echo "\n" | sudo apt-add-repository ppa:ondrej/php
 sudo apt update
 sudo apt install -y php8.0 libapache2-mod-php8.0
+sudo apt install libapache2-mod-php php-mysql
+sudo apt-get install -y php-pear
 
 # INSTALL APACHE
 sudo apt-get install apache2 -y
