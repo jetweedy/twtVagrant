79cdec99811ce80b9022e5e8212b0066ebcdc560
Jonathan Tweedy <jetweedy@gmail.com>
2022-07-11 15:47:18 -0400

    m

--- resources/apache-setup.sh
+++ resources/apache-setup.sh
@@ -4,5 +4,5 @@
 sudo apt-get install apache2 -y
 sudo a2enmod rewrite
 sudo a2enmod cgi
-cp /vagrant/000-default.conf /etc/apache2/sites-available/000-default.conf
+cp /vagrant/000-default.conf /etc/apache2/sites-enabled/000-default.conf
 sudo service apache2 restart
